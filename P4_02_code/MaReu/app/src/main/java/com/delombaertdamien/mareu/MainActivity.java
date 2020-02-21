e9cf47bcf136552eec28fb18ffbcf499e9731bbc
Damien PULP <damibolo@gmail.com>
2020-02-21 13:00:37 +0100

    Création des premier layout ('MainActivity', 'Menu', 'Metting_item')

--- P4_02_code/MaReu/app/src/main/java/com/delombaertdamien/mareu/MainActivity.java
+++ P4_02_code/MaReu/app/src/main/java/com/delombaertdamien/mareu/MainActivity.java
@@ -1,8 +1,13 @@
 package com.delombaertdamien.mareu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        return true;
     }
 }
